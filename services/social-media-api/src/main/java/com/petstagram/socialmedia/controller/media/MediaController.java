1cf86b22d5b728cd248db105a77160fa4084099c
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-01-24 23:39:53 +0300

    grpc client implemented for social media api

---
+++ services/social-media-api/src/main/java/com/petstagram/socialmedia/controller/media/MediaController.java
@@ -0,0 +1,37 @@
+package com.petstagram.socialmedia.controller.media;
+
+import com.petstagram.socialmedia.FileType;
+import com.petstagram.socialmedia.GetResponse;
+import com.petstagram.socialmedia.UploadResponse;
+import com.petstagram.socialmedia.configuration.response.ServiceResponse;
+import com.petstagram.socialmedia.controller.base.BaseController;
+import com.petstagram.socialmedia.service.grpc.GrpcClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/media")
+public class MediaController extends BaseController {
+
+    private final GrpcClientService grpcClientService;
+
+    public MediaController(GrpcClientService grpcClientService) {
+        this.grpcClientService = grpcClientService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ServiceResponse<GetResponse>> get(@PathVariable UUID id) {
+        return controllerResponse(grpcClientService.getFiles(id.toString(), FileType.POST_IMAGE));
+    }
+
+    @PostMapping
+    public ResponseEntity<ServiceResponse<UploadResponse>> upload(@RequestParam("file") MultipartFile file, @RequestParam("id") UUID id, @RequestParam("fileType") FileType fileType) throws IOException {
+            byte[] fileContent = file.getBytes();
+            ServiceResponse<UploadResponse> response = grpcClientService.uploadFiles(id, file.getOriginalFilename(), file.getContentType(), fileType, "post-images", fileContent);
+            return controllerResponse(response);
+    }
+}
--- services/social-media-api/src/main/java/com/petstagram/socialmedia/service/grpc/MediaGrpcClient.java
+++
@@ -1,7 +0,0 @@
-//package com.petstagram.socialmedia.service.grpc;
-//
-//import net.devh.boot.grpc.client.stubfactory.BlockingStubFactory;
-//
-//public class MediaGrpcClient {
-//    private final MediaGrpc
-//}
